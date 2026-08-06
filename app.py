dda67c7a2409097d7f0c420ea79b46ce2b290ea4
burn00s <burn00s@outlook.com>
2026-08-06 02:01:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ PROJECT_URL = os.environ.get('PROJECT_URL', '')        ,
 AUTO_ACCESS = os.environ.get('AUTO_ACCESS', 'false').lower() == 'true'  
 FILE_PATH = os.environ.get('FILE_PATH', './.cache')   
 SUB_PATH = os.environ.get('SUB_PATH', 'sub')           
-UUID = os.environ.get('UUID', '651055d2-c75c-4e7d-9e45-4d4d9f9ba508')  
+UUID = os.environ.get('UUID', 'b0a6b602-3c2a-4585-ae15-e8befb4b3581')  
 NEZHA_SERVER = os.environ.get('NEZHA_SERVER', '')     
 NEZHA_PORT = os.environ.get('NEZHA_PORT', '')          
 NEZHA_KEY = os.environ.get('NEZHA_KEY', '')           
